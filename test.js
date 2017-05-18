82b50af5b2149d2f5bc75066ad2d92a5882a79ac
Sindre Sorhus <sindresorhus@gmail.com>
2017-05-18 16:31:33 +0700

    Cleanup in test

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
+import fs from 'fs';
 import test from 'ava';
 import execa from 'execa';
 
 test(async t => {
+	try {
+		fs.unlinkSync('.last-photo');
+	} catch (err) {}
+
 	await t.notThrows(execa('./cli.js'));
 });
